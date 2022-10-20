365a7f614d57b02cefec53ec88031725970a30f4
greydevv <greysonmurray.dev@gmail.com>
2022-10-20 07:39:32 -0400

    Initial rendering of custom 404

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+
+export default function Custom404() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <h1 className="ibm-plex-sans font-bold w-full text-center tracking-widest text-8xl sm:text-[10rem]">
+        404
+      </h1>
+    </div>
+  )
+}
