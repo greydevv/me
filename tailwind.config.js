3dd1787dd9aa1b4c1962f0dc1f8bea9ed178c78e
greydevv <greysonmurray.dev@gmail.com>
2022-11-28 11:11:28 -0500

    Extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/mjsx.js",
     "./src/pages/**/*.js",
     "./src/components/**/*.js",
     "./src/layouts/**/*.js",
@@ -9,13 +10,15 @@ module.exports = {
     extend: {
       fontFamily: {
         'ibm-plex-sans': ['IBM Plex Sans', 'sans-serif'],
-        'sans': ['Inter', 'sans-serif']
+        'sans': ['Inter', 'sans-serif'],
+        'dm-mono': ['DM Mono, sans-serif']
       },
       colors: {
         'dark': '#363636',
         'light': '#F5F5F5',
         'grey': '#ACACAC',
         'grey-10': '#D4D4D4',
+        'grey-20': '#E8E8E8',
       
         'red': '#8F6D6D',
         'red-10': '#E9DFDF',
