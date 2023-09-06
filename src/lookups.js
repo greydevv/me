e731ec9c1d4e1d3edc06b65b97b327e8d8cbba46
greydevv <greysonmurray.dev@gmail.com>
2023-09-06 17:41:34 -0400

    Update about me text

--- src/lookups.js
+++ src/lookups.js
@@ -2,7 +2,7 @@ const INTRO = "Hey! I was born in Baltimore, Maryland, and raised in Southern Yo
 
 const ABOUT_ME = [
   "From a young age, I’ve always been fascinated by art. I spent much of my childhood drawing and painting, in and out of art classes. There is something about creating something from nothing that resonates with me.",
-  "When I was introduced to my first computer at a young age, I was hooked – I instantly found ways to combine my love for art and new obsession with technology. Hours were spent programming simple video games on Scratch and listing them publicly for others to play.",
+  "When I was introduced to my first computer, I was hooked – I instantly found ways to combine my love for art and new obsession with technology. Hours were spent programming simple video games on Scratch and listing them publicly for others to play.",
   "In high school, I picked up 3D modeling and VFX in Blender. Intrigued by the extensibility of Blender, I found myself motivated to enhance my experience and began learning Python with the intent of developing Blender plugins. For me, that was the catalyst into computer science. Since then, computer science has become my passion, study, and career.",
 ]
 
